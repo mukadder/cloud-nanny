851e7333760baafa532dce6d849b9fe47c130dcd
Per Bockman <per.bockman@gmail.com>
2016-03-07 06:53:05 +0100

    Adjusted scheduling time in example

--- upsert-rules.sh
+++ upsert-rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-# stop at 22:59 and start at 06:01
-BEDTIME_RULE_ARN=$(aws events put-rule --name Nanny-BedTime --description "Bedtime signal" --schedule-expression 'cron(59 21 * * ? *)' --query RuleArn --output text)
-WAKEUP_RULE_ARN=$(aws events put-rule --name Nanny-WakeUp --description "Wakeup signal" --schedule-expression 'cron(1 5 * * ? *)' --query RuleArn --output text)
+# stop at 21:55 and start at 06:00
+BEDTIME_RULE_ARN=$(aws events put-rule --name Nanny-BedTime --description "Bedtime signal" --schedule-expression 'cron(55 21 * * ? *)' --query RuleArn --output text)
+WAKEUP_RULE_ARN=$(aws events put-rule --name Nanny-WakeUp --description "Wakeup signal" --schedule-expression 'cron(0 6 * * ? *)' --query RuleArn --output text)
